37e0fcf009b8c33b69208747bb8be9aa88053411
sarwm-at-tu-chemnitz.de <sarwar.morshad@s2020.tu-chemnitz.de>
2026-05-21 10:14:48 +0200

    modular pattern

---
+++ src/db/index.ts
@@ -0,0 +1,27 @@
+import { Pool } from "pg";
+import config from "../config";
+
+// pool
+export const pool = new Pool({
+  connectionString: config.connection_String,
+});
+
+export const initDB = async () => {
+  try {
+    await pool.query(`
+  CREATE TABLE IF NOT EXISTS users(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50),
+    email VARCHAR(100) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    is_active BOOLEAN DEFAULT true,
+    age INT,
+    created_at TIMESTAMP DEFAULT NOW(),
+    updated_at TIMESTAMP DEFAULT NOW()
+  )
+`);
+    console.log("Database Connection Successful");
+  } catch (error) {
+    console.log(error);
+  }
+};
